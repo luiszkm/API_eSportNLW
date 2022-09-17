9bb6872541361e98e06bdcf006dd905bc3d4de5d
Luis Murilo <luiszkm@gmail.com>
2022-09-16 21:47:06 -0300

    api as finished

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,26 @@
 import express from 'express';
+import cors from 'cors';
+
 import { PrismaClient } from '@prisma/client';
+import {convertHourStringToMinutes} from './utils/convertHourStringToMinutes';
+import {convertMinutesToHoursString} from './utils/convertMinutesToHoursString';
+
 
 const app = express();
+app.use(cors({
+  origin: 'https://localhost:3000'
+}))
+
+app.use(express.json())
 const prisma = new PrismaClient({
-  log:['query']
+  log: ['query']
 })
 
 app.get('/games', async (req, res) => {
   const games = await prisma.game.findMany({
-    include:{
-      _count:{
-        select:{
+    include: {
+      _count: {
+        select: {
           ads: true,
         }
       }
@@ -18,6 +28,79 @@ app.get('/games', async (req, res) => {
   })
 
   return res.json(games)
+})
+
+app.get('/games/:id/ads', async (req, res) => {
+  const gameId = req.params.id
+
+  const ads = await prisma.ad.findMany({
+    select: {
+      id: true,
+      name: true,
+      weekDays: true,
+      useVoiceChanel: true,
+      yearsPlaying: true,
+      hourStart: true,
+      hourEnd: true,
+    },
+    where: {
+      gameId,
+    },
+    orderBy: {
+      created_at: 'desc',
+    }
+  })
+
+  return res.json(ads.map(ad => {
+    return {
+      ...ad,
+      weekDays: ad.weekDays.split(','),
+      hourStart: convertMinutesToHoursString(ad.hourStart),
+      hourEnd: convertMinutesToHoursString(ad.hourEnd),
+    }
+  }))
+})
+
+app.get('/ads/:id/discord', async (req, res) => {
+  const adId = req.params.id
+
+  const ad = await prisma.ad.findUniqueOrThrow({
+    select: {
+      discord: true,
+    },
+    where: {
+      id: adId,
+    }
+  })
+
+  return res.json({
+    discord: ad.discord
+  })
+
+})
+
+
+app.post('/games/:id/ads', async (req, res) => {
+  const gameId = req.params.id
+  const body: any = req.body
+
+
+  const ad = await prisma.ad.create({
+    data: {
+      gameId,
+      name: body.name,
+      yearsPlaying: body.yearsPlaying,
+      discord: body.discord,
+      weekDays: body.weekDays.join(','),
+      hourStart: convertHourStringToMinutes(body.hourStart),
+      hourEnd: convertHourStringToMinutes(body.hourEnd),
+      useVoiceChanel: body.useVoiceChanel
+
+
+    }
+  })
+  return res.status(201).json(ad)
+
 })
 
 
